e2995612127bd842bd5c314796fbfb1280e5b603
DESKTOP-0O0KUAD\msi <hyq961206@sjtu.edu.cn>
2018-07-01 17:00:05 +0800

    try ribbon

--- feign/src/main/java/com/example/demo/Service/HiService.java
+++ feign/src/main/java/com/example/demo/Service/HiService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value="service.hi",fallback = HiServiceHystricImpl.class)
+@FeignClient(value="service-hi",fallback = HiServiceHystricImpl.class)
 @Service
 public interface HiService {
 
